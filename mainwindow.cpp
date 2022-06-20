178d3bf2612668781e218e1909a4b6761c4eacdd
cenancaymaz <107841359+cenancaymaz@users.noreply.github.com>
2022-06-20 11:52:12 +0300

    First commit.

---
+++ mainwindow.cpp
@@ -0,0 +1,14 @@
+#include "mainwindow.h"
+#include <QDebug>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+{
+    qDebug()<<"Deneme";
+    qDebug()<<"Hasan";
+}
+
+MainWindow::~MainWindow()
+{
+}
+
